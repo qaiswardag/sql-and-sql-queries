ad327046fbb0439493939bce5a6526be43a554a3
qaiswardag <qais.wardag@outlook.com>
2022-05-15 20:16:38 +0200

    Add insert-into-tables

---
+++ insert-into-tables.sql
@@ -0,0 +1,13 @@
+INSERT INTO cats(name, age, description)
+VALUES
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek'),
+('tommy', 7, 'lorem ipsium oi koi lek');
